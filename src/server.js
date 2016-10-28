b122a56dc5c1c15ba26c6437b886c7ad5948b93b
Quentin Headen <qheaden@phaseshiftsoftware.com>
2016-10-28 00:52:32 -0400

    Added the about page and a link to the blog.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ var express = require("express");
 
 // Get the controllers.
 var IndexController = require(__dirname + "/controllers/IndexController.js");
+var AboutController = require(__dirname + "/controllers/AboutController.js");
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
@@ -9,5 +10,6 @@ app.use(express.static(__dirname + "/../bower_components"));
 
 // Register the controllers.
 new IndexController(app, __dirname + "/templates");
+new AboutController(app, __dirname + "/templates");
 
 app.listen(8080, () => console.log("Listening on port 8080."));
